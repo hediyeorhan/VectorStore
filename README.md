ba72686ccf67e98fd891118f55d2c813f2e54cff
hediyeorhan <hediyeorhan2015@gmail.com>
2025-02-21 16:18:32 +0300

    vector projesi olusturuldu

---
+++ README.md
@@ -0,0 +1,48 @@
+# VectorStore
+
+Bu çalışmada Google AI tarafından geliştirilen yapay zekâ Gemini API'ı kullanılarak vector store ile bir proje geliştirilmiştir. 
+
+Projede __.env__ dosyasında içeriğinde şu veriler bulunmaktadır.
+
+• GEMINI_API_KEY=
+
+• LANGCHAIN_API_KEY=
+
+• LANGCHAIN_TRACING_V2=true
+
+• LANGCHAIN_PROJECT=PROJECT_NAME
+
+Projede, Gemini AI ile birlikte Langchain framework'ü kullanılmıştır. Langchain, büyük dil modelleri ile uygulama geliştirilmesinde kullanılmaktadır. Zincir yapısında LLM'lerin birbirleri ile ve insanlar ile konuşmasını sağlamaktadır. Döküman okuma, yükleme, embedding işlemleri ve vektör database işlemleri için langchain framework'ünden faydalanılmıştır. LangChain, LLM'ler ile entegrasyon sağlayarak özelleştirilmiş sorgu yönetimi sunmaktadır.
+
+<h3> Vector Store </h3>
+
+<br>
+Elimizde birçok veri / bilgi olduğunda en alakalı / mantıklı cevabı vermeyi sağlamaktadır. Vektörleştirme işlemi için ayrı embedding modelleri kullanılmaktadır.  Bu vektörler vector store'da kayıt edilmektedir.
+
+Bu çalışmada embedding fonksiyonu olarak __HuggingFaceEmbeddings__ kullanılmıştır.
+<br>
+Çalışmada vektör veri tabanı olarak Chroma kullanılmıştır. Chroma open source bir vektör veri tabanıdır.
+<br>
+Kullanılan dökümanlar kod içerisinde de tanımlanabilmektedir. Bir dosyadan da yüklenebilmektedir. Her iki örnek kod dosyasında mevcuttur.
+
+<br>
+
+Çalışmada langchain kullanılarak temel bir RAG mantığı kodlanmıştır. LLM'ler vector store kullanarak özelleştirilmiştir. Böylece kendi dökümanımızdaki veriler okunarak LLM'in istenilen cevapları vermesi sağlanmaktadır. Vector store'lar __similarity search__ ile dökümandaki benzer veriyi çıktı olarak sunmaktadır.
+
+Çalışmanın örnek çıktıları Şekil 1 ve Şekil 2'de görülmektedir.
+
+<br>
+<div align="center">
+<img src="https://github.com/user-attachments/assets/c5256344-4110-45ec-bc35-b37c2d3e7310" alt="image">
+</div>
+Şekil 1. Örnek çıktı
+
+<br>
+<br>
+
+
+<div align="center">
+<img src="https://github.com/user-attachments/assets/a316d309-76ab-4f7c-b7d1-319eef947d08" alt="image">
+</div>
+
+Şekil 2. Örnek çıktı
---
+++ vectorstore.py
@@ -0,0 +1,76 @@
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from langchain_core.documents import Document
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.document_loaders import PyMuPDFLoader
+from langchain_chroma import Chroma
+from langserve import add_routes
+from dotenv import load_dotenv
+from typing import List
+import os
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-2.0-flash", api_key = os.getenv("GEMINI_API_KEY"))
+
+# 1.secenek : dokuman icerigi bu sekilde kod icerisine gomulerek verilebilir.
+"""
+documents = [
+    Document(
+        page_content="Osmanlı İmparatorluğu, 1299 yılında Osman Gazi tarafından kurulmuş ve 600 yılı aşkın bir süre hüküm sürmüştür.",
+        metadata={"source": "osmanli-tarihi"},
+    ),
+    Document(
+        page_content="Malazgirt Meydan Muharebesi, 1071 yılında gerçekleşmiş ve Anadolu'nun kapılarını Türklere açmıştır.",
+        metadata={"source": "selcuklu-tarihi"},
+    ),
+    Document(
+        page_content="Cumhuriyet, 29 Ekim 1923'te ilan edilmiş ve Türkiye'nin yönetim şekli olarak kabul edilmiştir.",
+        metadata={"source": "turkiye-cumhuriyeti"},
+    ),
+    Document(
+        page_content="İstanbul, 1453 yılında II. Mehmet tarafından fethedilmiş ve Osmanlı İmparatorluğu’nun başkenti olmuştur.",
+        metadata={"source": "osmanli-tarihi"},
+    ),
+    Document(
+        page_content="Türkler, Orta Asya'dan batıya doğru göç ederek tarih boyunca birçok devlet kurmuşlardır.",
+        metadata={"source": "orta-asya-turkleri"},
+    ),
+]
+"""
+
+# 2.secenek : dokuman icerigi bir dosyadak yuklenebilir.
+
+file_path = "documents.pdf"
+
+loader = PyMuPDFLoader(file_path)
+documents = loader.load()
+
+
+vectorstore = Chroma.from_documents(
+    documents,
+    embedding=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2"),
+)
+
+retriever = RunnableLambda(vectorstore.similarity_search).bind(k=1)
+
+message = """
+Bu soruyu yalnızca verilen bağlamı kullanarak cevaplayın.
+
+{question}
+
+Context:
+{context}
+"""
+
+prompt = ChatPromptTemplate.from_messages([("human", message)])
+
+rag_chain = {"context": retriever, "question": RunnablePassthrough()} | prompt | model
+
+if __name__ == "__main__":
+    text = input("Lütfen bilgi almak istediğiniz konuyu söyler misiniz : ")
+    response = rag_chain.invoke(text)
+    print(response.content)
